2533cf94c1deb3a4a300a55119df0ac9df921dd3
Kevin Sekgobela <kevinnoko23@gmail.com>
2022-11-17 10:13:33 +0200

    Defined analyse functions

--- src/analyse.c
+++ src/analyse.c
@@ -1,2 +1,52 @@
 #include <analyse.h>
-#include <analyse.h>
+#include <util.h>
+
+#include <time.h>
+#include <stdlib.h>
+
+
+StudentsInfo getDefaultStudentsInfo(){
+    StudentsInfo students_info;
+    struct tm *current_time = presentTime();
+    year_t current_year = current_time->tm_year;
+    students_info.start_year = START_YEAR;
+    students_info.end_year = current_year + 1;
+    students_info.min_year = MIN_YEAR;
+    students_info.start_pos = START_POS;
+    students_info.end_pos = END_POS;
+    free(current_time);
+}
+
+capacity_t calYearCapacity(pos_t start_pos, pos_t end_pos){
+    // Calculates year capacity from year start and last position
+    // end_pos = (year_capacity + start_pos) - 1
+    // year_capacity = end_pos - start_pos + 1
+    if(start_pos == end_pos){
+        return 0;
+    }
+    return (end_pos - start_pos) + 1;
+}
+
+pos_t calEndpos(pos_t start_pos, capacity_t year_capacity){
+    // Calculates last position for student numbers
+    if(year_capacity == 0){
+        return start_pos;
+    }
+    return (year_capacity + start_pos) - 1;
+}
+
+size_t posPartSize(pos_t start_pos, capacity_t year_capacity){
+    // Calculates size(digits) position part of student number.
+    pos_t end_pos = calEndpos(start_pos, year_capacity);
+    return intSize(end_pos);
+}
+
+size_t yearPartSize(year_t year, unsigned int strict){
+    // Calculates size(digits) of year part of student number.
+    if(strict && getCentury(year)==20){
+        return 2;
+    }else{
+        return intSize(year);
+    }
+}
+
--- src/include/analyse.h
+++ src/include/analyse.h
@@ -3,7 +3,7 @@
 // Year UL started to operate
 #define START_YEAR 1959;
 // End year is based on current year plus 1
-// define END_YEAR = current_year()-> + 1;
+//#define END_YEAR = presentTime()->year + 1;
 
 // Minimum supported year
 #define MIN_YEAR 1000
@@ -14,11 +14,11 @@
 #define BEFORE_Y2K_YEAR Y2K_YEAR - 1;
 
 // First position likely 0
-#define YEAR_START_POS 0
+#define START_POS 0
 // Max capacity for a each year
 #define YEAR_CAPACITY 100000
 // Last/max end position for year
-#define YEAR_END_POS YEAR_CAPACITY + YEAR_START_POS - 1
+#define END_POS YEAR_CAPACITY + START_POS - 1
 
 
 typedef unsigned int year_t;
@@ -26,7 +26,7 @@ typedef unsigned int pos_t;
 typedef unsigned int capacity_t;
 typedef unsigned int size_t;
 
-struct year_data{
+struct StudentsInfo{
     year_t start_year;
     year_t end_year;
     year_t min_year;
@@ -35,6 +35,11 @@ struct year_data{
     pos_t end_pos;
     unsigned int strict;
 };
+
+typedef struct StudentsInfo StudentsInfo;
+
+StudentsInfo getDefaultStudentsInfo();
+
 
 /**
  * @brief Gets end year from local time year as (current year + 1)
@@ -49,4 +54,4 @@ pos_t calEndpos(pos_t start_pos, capacity_t year_capacity);
 
 size_t posPartSize(pos_t start_pos, capacity_t year_capacity);
 
-size_t yearPartSize(year_t start_year, year_t end_year);
+size_t yearPartSize(year_t year, unsigned int strict);
