b8a6edc4221e0f80f58356748caf319f3461d558
Kevin Sekgobela <kevinnoko23@gmail.com>
2022-11-23 22:10:35 +0200

    Update cmake files

--- tests/main.c
+++ tests/main.c
@@ -3,16 +3,17 @@
 #include <stdlib.h>
 
 #include <check.h>
-#include <test_util.h>
-#include <test_analyse.h>
+#include <tests.h>
 
 int main(void)
  {
    Suite *suite_utils = create_utils_suite();
    Suite *suite_analyse = create_analyse_suite();
+   Suite *suite_generate = create_generate_suite();
    SRunner *suite_runner = srunner_create(suite_utils);
 
    srunner_add_suite(suite_runner, suite_analyse);
+   srunner_add_suite(suite_runner, suite_generate);
 
    srunner_run_all(suite_runner, CK_NORMAL);
 
